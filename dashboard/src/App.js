dc436efc7bf8ec1af2d7c2329c13171f94de3f86
Tanuj Soni <tanuj32soni@gmail.com>
2019-11-11 12:46:38 +0530

    feat: initial setup for material-ui

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Desktop from '@material-ui/icons/DesktopMac';
 
 const App = () => (
-  <div>
-    <h3>React SPA</h3>
-  </div>
+  <Box display="flex">
+    <Desktop fontSize="large" /> 
+    <Typography variant="h5">
+      React SPA
+    </Typography>
+  </Box>
 );
 
 export default App;
