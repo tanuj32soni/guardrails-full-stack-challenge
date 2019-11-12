fedd3deea5dc8234a9ae1562e9e56bc9a1d5652d
Tanuj Soni <tanuj32soni@gmail.com>
2019-11-12 17:29:37 +0530

    feat: dockerize api server

--- dashboard/src/sagas/api.saga.js
+++ dashboard/src/sagas/api.saga.js
@@ -3,6 +3,8 @@ import { takeEvery, put } from 'redux-saga/effects';
 import { CALL_API } from '../actions/api.actions';
 import config from '../config';
 
+console.log(config.getProperties())
+
 const getHeaders = () => {
   const headers = {
     Accept: 'application/json',
