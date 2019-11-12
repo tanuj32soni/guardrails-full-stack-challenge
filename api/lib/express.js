e3482f039bb2667a66a544bf438772acdbc2bb11
Tanuj Soni <tanuj32soni@gmail.com>
2019-11-12 12:40:21 +0530

    feat: added extra express handlers
    - initPingUrl: Provides ping functionality
    
    - catchNotFound: Respondsto Incorrect Routes
    
    - catchErrorRoutes: Handles errors thrown by other routes

--- api/lib/express.js
+++ api/lib/express.js
@@ -3,9 +3,35 @@ const initRoutes = require('../app/routes')
 
 const app = express()
 
+function initPingURL () {
+  app.get('/_ping', (req, res) => {
+    res.status(200).send({ result: 'Ping Received!!!' })
+  })
+}
+
+function catchNotFound () {
+  app.use((req, res) => res.status(400).send({ reason: 'Not Found' }))
+}
+
+function catchErrorRoutes () {
+  app.use((err, req, res, next) => {
+    if (!err) return next()
+    return res.status(400).send({ reason: err.message })
+  })
+}
+
 function init () {
+  // Initialize Ping URL
+  initPingURL()
+
   // Initialize API Routes
   initRoutes(app)
+
+  // Initialize Not Found Route
+  catchNotFound()
+
+  // Initialize Error Routes
+  catchErrorRoutes()
 
   return app
 }
