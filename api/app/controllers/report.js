5d8dfb862c8f7815ab3e9d8e03b7ef97e1794aad
Tanuj Soni <tanuj32soni@gmail.com>
2019-11-12 14:52:54 +0530

    feat: added create scan functionality

--- api/app/controllers/report.js
+++ api/app/controllers/report.js
@@ -1,8 +1,30 @@
+const { Report } = require('./../models')
+const validations = require('./../validations/schema')
 const Responder = require('../../lib/expressResponder')
+const BadRequestError = require('../errors/badRequestError')
 
 class ReportController {
   static async add (req, res) {
-    Responder.created(res, 'Report Created')
+    const schema = validations.createScan()
+    const { error, value } = schema.validate(req.body)
+
+    const reportDetails = value
+
+    if (error) {
+      const reason = error.details[0].message.replace(new RegExp('"', 'g'), "'")
+      return Responder.operationFailed(res, new BadRequestError(reason))
+    }
+
+    const reportExists = await Report.count({ where: { repository_name: req.body.repository_name } })
+    if (reportExists) {
+      return Responder.operationFailed(res, new BadRequestError('Report is already Registered!'))
+    }
+
+    const report = new Report(reportDetails)
+
+    await report.save()
+
+    Responder.created(res, { status: 'Report Created' })
   }
 
   static async list (req, res) {
