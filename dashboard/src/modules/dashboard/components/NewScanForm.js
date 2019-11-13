cecddf4058b45fe82671840eee38fd78c4c8abff
Tanuj Soni <tanuj32soni@gmail.com>
2019-11-13 19:07:44 +0530

    feat: add scan form validation

--- dashboard/src/modules/dashboard/components/NewScanForm.js
+++ dashboard/src/modules/dashboard/components/NewScanForm.js
@@ -67,39 +67,70 @@ const NewScanForm = ({
   submitting,
   handleSubmit,
 }) => (
-  <Form onSubmit={handleSubmit}>
-    <Box component={Paper} p={2}>
-      <TextField label="Repository Name" name="repository_name" />
-      <Select label="Status" name="status" options={statusOptions} />
-      <Box display="flex" justifyContent="space-between" marginY={2}>
-        <DatePicker label="Queued At" name="queued_at" />
-        <DatePicker label="Scanning At" name="scanning_at" />
-        <DatePicker label="Finished At" name="finished_at" />
-      </Box>
-      <FieldArray
-        name="findings"
-        label="Finding"
-        addButtonText="Add finding"
-        removeButtonText="Remove"
-        fields={findingFields}
-      />
-      <Box display="flex" justifyContent="space-between" marginY={2}>
-        <SubmitButton
-          buttonText="Create Scan"
-          pristine={pristine}
-          submitting={submitting}
+    <Form onSubmit={handleSubmit}>
+      <Box component={Paper} p={2}>
+        <TextField label="Repository Name" name="repository_name" />
+        <Select label="Status" name="status" options={statusOptions} />
+        <Box display="flex" justifyContent="space-between" marginY={2}>
+          <DatePicker label="Queued At" name="queued_at" />
+          <DatePicker label="Scanning At" name="scanning_at" />
+          <DatePicker label="Finished At" name="finished_at" />
+        </Box>
+        <FieldArray
+          name="findings"
+          label="Finding"
+          addButtonText="Add finding"
+          removeButtonText="Remove"
+          fields={findingFields}
         />
-        <FormActionButton
-          buttonText="Reset"
-          pristine={pristine}
-          submitting={submitting}
-          onClick={reset}
-        />
+        <Box display="flex" justifyContent="space-between" marginY={2}>
+          <SubmitButton
+            buttonText="Create Scan"
+            pristine={pristine}
+            submitting={submitting}
+          />
+          <FormActionButton
+            buttonText="Reset"
+            pristine={pristine}
+            submitting={submitting}
+            onClick={reset}
+          />
+        </Box>
       </Box>
-    </Box>
-  </Form>
-);
+    </Form>
+  );
+
+const validate = values => {
+  const errors = {};
+  const requiredFields = [
+    'repository_name',
+    'status',
+    'queued_at',
+    'scanning_at',
+    'finished_at'
+  ];
+
+  // Check for required fields
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required';
+    }
+  });
+
+  if (values['repository_name'] && values['repository_name'].replace(/\s/g, "") == "") {
+    errors['repository_name'] = "Repository name can't be blank";
+  }
+  if (values['queued_at'] && values['scanning_at'] && (new Date(values['queued_at']) > new Date(values['scanning_at']))) {
+    errors['scanning_at'] = "Error: Can't be scanned before it is queued";
+  }
+  if (values['scanning_at'] && values['finished_at'] && (new Date(values['scanning_at']) > new Date(values['finished_at']))) {
+    errors['finished_at'] = "Error: Can't be finished before it is scanned";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: 'newScanForm',
+  validate
 })(NewScanForm);
